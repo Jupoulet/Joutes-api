fbc7049ae6a0dca3f0e03e1c3d1a05b3d24a8e25
Julien Picard <julienpicard.iws@gmail.com>
2019-10-26 01:59:01 +0200

    coucou

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT;
 
 app.get('/hello', function (req, res) {
   res.json({ message: 'Hello World!'})
 })
 
-app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`)
+app.listen(process.env.PORT || 4000, function () {
+  console.log(`Example app listening on port ${process.env.PORT || 4000}!`)
 })
